8c9e1284511d1ebb9dc0d41d6067f3389f4e858a
Nah~ <110066948+nancyuzawa@users.noreply.github.com>
2023-09-08 19:59:06 -0300

    Create Aumento do salario.c

---
+++ Codes/Modulo 5 - Estruturas de Selecao/Aumento do salario.c
@@ -0,0 +1,36 @@
+/*Calculando o aumento de um salário na qual:
+Menor que R$500,00 -> aumento de 5%
+De R$500,00 (inclusive) a R$850,00 (inclusive)-> aumento de 10%
+acima de R$850,00 -> aumento de 15%
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    float salario, aumento, novoSalario;
+
+    printf("Salario: R$ ");
+    scanf("%f", &salario);
+
+    if (salario <500,00){
+        aumento = salario * 5 / 100;
+    }
+    else if (salario >= 500,00 || salario <=850,00){
+        aumento = salario * 10 / 100;
+    }
+    else{
+        aumento = salario * 15 / 100;
+    }
+
+    printf("\nAumento          R$ %.2f", aumento);
+
+    novoSalario = salario + aumento;
+
+    printf("\nNovo salário     R$ %.2f\n", novoSalario);
+
+    return 0;
+}
